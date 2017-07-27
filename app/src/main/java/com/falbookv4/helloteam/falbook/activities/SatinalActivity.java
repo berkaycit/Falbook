3f5f03b305d7fca2c0c60cf8f0469b9332e571e4
berkaycit <berkay.cit@std.hku.edu.tr>
2017-07-28 00:33:35 +0300

    cache düzenleme

--- app/src/main/java/com/falbookv4/helloteam/falbook/activities/SatinalActivity.java
+++ app/src/main/java/com/falbookv4/helloteam/falbook/activities/SatinalActivity.java
@@ -309,7 +309,6 @@ public class SatinalActivity extends AppCompatActivity implements RewardedVideoA
     @Override
     public void onDestroy() {
         mAd.destroy(this);
-        deleteCache(this);
 
         if(billingProcessor != null){
             billingProcessor.release();
@@ -352,29 +351,6 @@ public class SatinalActivity extends AppCompatActivity implements RewardedVideoA
 
     }
 
-    public static void deleteCache(Context context) {
-        try {
-            File dir = context.getCacheDir();
-            deleteDir(dir);
-        } catch (Exception e) {}
-    }
-
-    public static boolean deleteDir(File dir) {
-        if (dir != null && dir.isDirectory()) {
-            String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-            return dir.delete();
-        } else if(dir!= null && dir.isFile()) {
-            return dir.delete();
-        } else {
-            return false;
-        }
-    }
 
 
 }
